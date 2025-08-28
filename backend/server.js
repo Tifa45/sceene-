227c1ddb865fc199937a83d2e18c9bc82cf3daaf
Mostafa Yousry <m.yousry8806@gmail.com>
2025-08-29 01:48:07 +0300

    small comma

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ configDotenv();
 const app = express();
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173"
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
     methods: ["GET", "POST", "DELETE", "PUT", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
