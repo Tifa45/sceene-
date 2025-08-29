53f7bb6378c274d68fbaf816ef92b0ea2658e3b3
Mostafa Yousry <m.yousry8806@gmail.com>
2025-08-29 03:15:15 +0300

    another update on get show details

--- frontend/src/pages/show-details.jsx
+++ frontend/src/pages/show-details.jsx
@@ -32,7 +32,7 @@ function ShowDetails() {
   async function getShow() {
     setLoading((prev) => ({ ...prev, main: true }));
     try {
-      const response = await axios.get(`/shows/one-show/${id}`);
+      const response = await api.get(`/shows/one-show/${id}`);
       setCurrentShow(response.data.showsData);
       setErrMsg((msg) => ({ ...msg, main: null }));
     } catch (error) {
